26e8d8eca5b5a8e180ea749ba12ce7d37e009779
thegoldengoblin <69628636+thegoldengoblin@users.noreply.github.com>
2022-02-17 22:50:13 +0530

    updated validation.js
    validation script changed as per requirement

--- validation.js
+++ validation.js
@@ -1,36 +1,38 @@
-function validatemail()
+function validationOfMail(mail)
 {
-	event.preventDefault();
-	var email = /^([a-zA-Z0-9\.-]{4,20})@([a-zA-Z0-9\-]{2,20}).([a-z]{2,5})(.[a-z]{2,5})?$/.test(mail);
+	//validating e mail with a condition of:must be min of 4 digit and max is 20 and there must be @ between and after that 2 min letter and 5 max '.'oaparator and after min is 2 and max is 4 
+	  return /^([a-zA-Z0-9\.-]{4,20})@([a-zA-Z0-9\-]{2,20}).([a-z]{2,5})(.[a-z]{2,5})?$/.test(userEmail);
 }
 
-function validatepassword()
+function validationOfPassword(userPassword)
 {
-	
- var Password =	/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/.test(Pass);
+	//Password matches only when it has atleast one uppercase one special char and one number
+   return	/^(?=.*[0-9])(?=.*[!@%^&*])[a-zA-Z0-9!@%^&*]{7,15}$/.test(userPassword);
 }
 	
-function validatephone()
+function validationOfContact(userContactNumber)
 {	
-
-var phone=/^[6789]\d{9}$/.test(phone);
-}
-
-function validatename()
-{
-
-var name=/^[A-Za-z\s]{25}$/.test(name);
-}
-
-function validatecity()
-{
-
-var city=/^[a-zA-z\s]{30} ?([a-zA-z]|[a-zA-z] )*[a-zA-z]$/.test(city)
+	//validates indian telephone number and indian moblie number
+	return /^([0-9]{5}[-\s][0-9]{6}||[6789]\d{9})$/.test(userContactNumber);
 
 }
 
-function validatepin()
+function validationOfName()
 {
-var pin=/^[0-9]{6}$/.test(pin)
+	//validates any name having minimum of 2 and maximum of 20 char length
+	return /^[A-Za-z\s]{2,20}$/.test(userName);
+}
+
+function validationOfCity()
+{
+	//validates any name with space and '.' with maximum length of 30
+	return /^[a-zA-Z\s.]{3,30}$/.test(userCity);
+
+}
+
+function validationOfPincode()
+{
+	//validates indian pincode maxim leength is 6
+	return /^[0-9]{6,6}$/.test(userPincoe);
 
 }
